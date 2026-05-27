6876d2e405821efc2f4fb0da8c333e10c0362568
davidzaro <davidzaro494@gmail.com>
2026-05-27 13:40:26 +0900

    Add file logging regression test
    Covers shared formatter logging so colored console tracebacks do not leak into file logs.

---
+++ test/test_logging_file.py
@@ -0,0 +1,57 @@
+from __future__ import absolute_import
+
+import logging
+import os
+import tempfile
+
+import better_exceptions
+
+
+ANSI_ESCAPE = '\x1b['
+
+
+def explode():
+    marker = 'file logs should not be colorized'
+    raise RuntimeError(marker)
+
+
+def main():
+    path = os.path.join(tempfile.gettempdir(), 'better_exceptions_file_logging.log')
+    try:
+        os.remove(path)
+    except OSError:
+        pass
+
+    logger = logging.getLogger('better_exceptions.file_logging')
+    logger.handlers[:] = []
+    logger.propagate = False
+    logger.setLevel(logging.ERROR)
+
+    formatter = logging.Formatter('%(levelname)s:%(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    file_handler = logging.FileHandler(path, mode='w')
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    better_exceptions.hook()
+
+    try:
+        explode()
+    except RuntimeError:
+        logger.exception('shared formatter failure')
+
+    file_handler.close()
+
+    with open(path, 'r') as logfile:
+        logged = logfile.read()
+
+    assert ANSI_ESCAPE not in logged, logged
+
+
+if __name__ == '__main__':
+    main()
